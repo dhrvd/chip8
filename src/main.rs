1939fcdb407b4b17851cb38ca07037c65a1634aa
dhrvd <138230650+dhrvd@users.noreply.github.com>
2024-10-09 17:27:56 +0530

    window config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "CHIP8".to_owned(),
+        window_height: 320,
+        window_width: 640,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        next_frame().await
+    }
 }
